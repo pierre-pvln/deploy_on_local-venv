a0eb148b38fe267090c62b1881bf5c62e1125149
Pierre Veelen | aMultis.dev | _AWS_nester_reporting_excel_LAMBDA <pierre@amultis.dev>
2025-04-03 12:53:50 +0200

    added check for jupyter kernel added

--- jupyterlab3.cmd
+++ jupyterlab3.cmd
@@ -22,11 +22,29 @@ SET ERROR_MESSAGE=[%ME%] [ERROR] Unknown settings for COMPUTERNAME: %COMPUTERNAM
 GOTO ERROR_EXIT
 
 :LEGION-2020
+::ECHO debug jupyter
+::dir %APPDATA%\jupyter\kernels
+::ECHO ==============
+::dir %APPDATA%\jupyter\kernels\%venv_environment%
+::ECHO ==============
+::dir %APPDATA%\jupyter\kernels\%venv_environment%\kernel.json
+::ECHO ==============
+
+:: https://medium.com/@royce963/setting-up-jupyterlab-and-a-virtual-environment-c79002e0e5f7
+:: C:\Users\developer\AppData\Roaming\jupyter\kernels\.venv
+IF NOT EXIST %APPDATA%\jupyter\kernels\%venv_environment%\kernel.json (
+	SET ERROR_MESSAGE=[%ME%] [ERROR] %APPDATA%\jupyter\kernels\%venv_environment%\kernel.json not found ...
+	ECHO [%ME%] Activate the virtual environment %venv_environment%
+    ECHO [%ME%] and	execute command:
+	ECHO [%ME%] python -m ipykernel install --user --name=%venv_environment%
+	GOTO ERROR_EXIT
+)
 IF "%USERNAME%"=="developer" (
    echo Running jupyter lab for %USERNAME% on %COMPUTERNAME%
    echo for the %venv_environment% environment
    cd .\py_app
    call %venv_environment%\Scripts\activate.bat
+   
    call jupyter lab
    GOTO CLEAN_EXIT
 )
